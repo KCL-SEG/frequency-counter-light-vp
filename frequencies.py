fde96da5d0213642ecff82d8670a16db17b33eb5
Vaibhavkumar Patel <Vaibhavkumar.patel@kcl.ac.uk>
2022-09-28 13:50:50 +0100

    Frequencies completed

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(len(items)):
+        j = 0;
+        currItem = items[i]
+        while(currItem == items[i]):
+            j = j + 1
+            frequencies.update({currItem : j})
+        else:
+            currItem = items[i]
     return frequencies
+
+print(f' {frequencies( ['a','a'] ) } ')
